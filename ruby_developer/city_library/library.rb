e3ca5b6001b2490673992637f0e494bb2200c431
victor-cleber <13708226+victor-cleber@users.noreply.github.com>
2023-09-07 22:09:34 +1200

    Printing the visitor details

--- ruby_developer/city_library/library.rb
+++ ruby_developer/city_library/library.rb
@@ -2,6 +2,10 @@
 puts "----------------------------------------"
 puts "Thank you for visiting our City Library.\n "
 puts "Please complete the fields bellow: "
-puts "Name: "
-puts "Surname: "
-puts "Age: "
+print "Name: "
+name = gets.chomp
+print "Surname: "
+surname = gets.chomp
+print "Age: "
+age = gets.chomp
+puts "[1] #{name} #{surname} - #{age} anos"
