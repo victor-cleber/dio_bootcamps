798117d58773fb4c2cf4d788d7e8f592c0302c21
victor-cleber <13708226+victor-cleber@users.noreply.github.com>
2023-09-07 22:02:11 +1200

    Add the initial project printing messages

---
+++ ruby_developer/city_library/library.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby
+puts "----------------------------------------"
+puts "Thank you for visiting our City Library.\n "
+puts "Please complete the fields bellow: "
+puts "Name: "
+puts "Surname: "
+puts "Age: "
