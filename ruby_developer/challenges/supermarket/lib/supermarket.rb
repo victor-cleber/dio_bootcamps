46d3d9bdf5252ffbeeb8391fb6ede5212c1bd95e
victor-cleber <13708226+victor-cleber@users.noreply.github.com>
2023-10-21 22:44:43 +1300

    Add test for buy method

--- ruby_developer/challenges/supermarket/lib/supermarket.rb
+++ ruby_developer/challenges/supermarket/lib/supermarket.rb
@@ -1,6 +1,11 @@
 class Supermarket
-    attr_accessor :product
-    def initialize(product)
-        @product = product
-    end
+  attr_reader :product
+  def initialize(product)
+    @product = product
+  end
+
+  def comprar
+    "You bought a #{@product.name} costing R$#{"%.2f" % @product.price}"
+  end
+
 end
--- ruby_developer/challenges/supermarket/spec/supermarket_spec.rb
+++ ruby_developer/challenges/supermarket/spec/supermarket_spec.rb
@@ -3,12 +3,20 @@ require 'supermarket.rb'
 require 'product.rb'
 
 describe Supermarket do
-  describe "Supermarket initialized" do
-    it  'Supermarket is initialized with a Product' do
-        product = Product.new('Forma', 60.00)
+  describe do
+    it  '1. Supermarket is initialized with a Product' do
+        product = Product.new('Forma', 60)
         supermarket = Supermarket.new(product)
         expect(supermarket.product.name).to eq('Forma')
-        expect(supermarket.product.price).to eq(60.00)
+        expect(supermarket.product.price).to eq(60)
+    end
+  end
+
+  describe do
+    it  '2. A product was bought' do
+        product = Product.new('Forma', 60)
+        supermarket = Supermarket.new(product)
+        expect(supermarket.comprar).to eq("You bought a Forma costing R$60.00")
       end
   end
 end
