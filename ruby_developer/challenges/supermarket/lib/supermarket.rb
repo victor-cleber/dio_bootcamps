b378c3bb36fed3e4f3fa78e50e0e9f12ba06a322
victor-cleber <13708226+victor-cleber@users.noreply.github.com>
2023-10-21 22:14:24 +1300

    Add Supermarket tests

---
+++ ruby_developer/challenges/supermarket/lib/supermarket.rb
@@ -0,0 +1,6 @@
+class Supermarket
+    attr_accessor :product
+    def initialize(product)
+        @product = product
+    end
+end
--- ruby_developer/challenges/supermarket/spec/supermarket_spec.rb
+++ ruby_developer/challenges/supermarket/spec/supermarket_spec.rb
@@ -1,3 +1,14 @@
-# spec/string_calculator_spec.rb
+require 'rspec'
+require 'supermarket.rb'
+require 'product.rb'
+
 describe Supermarket do
+  describe "Supermarket initialized" do
+    it  'Supermarket is initialized with a Product' do
+        product = Product.new('Forma', 60.00)
+        supermarket = Supermarket.new(product)
+        expect(supermarket.product.name).to eq('Forma')
+        expect(supermarket.product.price).to eq(60.00)
+      end
+  end
 end
